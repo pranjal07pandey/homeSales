0753734df6a11f0a966aad28ee41ab05dd0dc951
Pranjal <pranjalpandey92@gmail.com>
2020-08-22 23:56:25 +0545

    lastest changes

--- sales/urls.py
+++ sales/urls.py
@@ -5,5 +5,13 @@ from . import views
 urlpatterns = [
     path('', views.index, name = 'index' ),
     path('home/<int:pk>', views.homeDetails, name = 'home-details' ),
+    path('all-homes/',views.allHomes, name='all-homes'),
+    path('agents/', views.agents, name='agents'),
+    path('agent/<int:pk>/', views.individualAgent, name='individual-agent'),
+    path('register/', views.registerUser, name = 'register'),
+    path('login/', views.loginPage, name = 'login'),
+    path('logout/', views.logoutUser, name = 'logout'),
+    path('dashboard/<int:pk>/', views.dashboard, name = 'dashboard'),
+
 
 ]
--- sales/views.py
+++ sales/views.py
@@ -1,13 +1,62 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.core.paginator import Paginator
 from .models import *
+from .forms import CreateUserForm
+from django.contrib.auth import authenticate, login, logout 
 
-# Create your views here.
+
+
+def registerUser(request):
+    if request.user.is_authenticated:
+        return redirect('index')
+    else:
+        form = CreateUserForm()
+        if request.method == 'POST':
+            form = CreateUserForm(request.POST)
+            if form.is_valid():
+                form.save()
+                return redirect('login')
+    
+        return render(request, 'sales/register.html',{
+            'form':form
+        })
+
+
+def loginPage(request):
+
+    if request.user.is_authenticated:
+        return redirect('index')
+    
+    else:
+        if request.method == 'POST':
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+
+            user = authenticate(request, username= username, password= password)
+
+            if user is not None:
+                login(request, user)
+                return redirect('index')
+
+        return render(request, 'sales/login.html',{
+
+    })
+
+def logoutUser(request):
+    logout(request)
+    return redirect('index')       
+
+
 def index(request):
     homes = Home.objects.all()
+    paginator = Paginator(homes,6)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
     agents = Agent.objects.count()
     print(agents)
     return render(request, 'sales/index.html',{
-        'homes': homes,
+        'homes': page_obj,
         'agents': agents
     })
 
@@ -18,3 +67,29 @@ def homeDetails(request, pk):
         'home': home
     })
 
+def allHomes(request):
+    homes = Home.objects.all()
+    return render(request, 'sales/houses.html',{
+        'homes':homes
+    })
+
+def agents(request):
+    agents = Agent.objects.all()
+    mvp = Agent.objects.filter(is_mvp=True)
+    return render(request, 'sales/agents.html',{
+        'agents': agents,
+        'mvp' : mvp
+    })
+
+def individualAgent(request, pk):
+    agent = Agent.objects.get(id=pk)
+    home = Home.objects.filter(agent=agent)
+    return render(request, 'sales/individualAgent.html',{
+        'agent':agent,
+        'home':home
+    })
+
+
+def dashboard(request, pk):
+    
+    return render(request, 'sales/dashboard.html')
